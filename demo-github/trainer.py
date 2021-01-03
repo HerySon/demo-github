2508da13657808d47872e602e328c877bdc7233f
HerySon <nicolas_rochet@hotmail.com>
2021-01-03 20:26:36 +0100

    add linear regression

--- demo-github/trainer.py
+++ demo-github/trainer.py
@@ -1,5 +1,7 @@
 from sklearn.preprocessing import StandardScaler
+from linear_model import LinearRegression
 
 class Trainer():
     def train():
         std = StandardScaler()
+        lr = LinearRegression()
