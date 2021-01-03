4eb4e9647782f50ce5ec112c3b973f4d350c6e10
HerySon <nicolas_rochet@hotmail.com>
2021-01-03 20:49:34 +0100

    add ohe

--- demo-github/trainer.py
+++ demo-github/trainer.py
@@ -1,5 +1,10 @@
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from linear_model import LinearRegression
+
 
 class Trainer():
     def train():
         std = StandardScaler()
+        ohe = OneHotEncoder()
+        lr = LinearRegression()
